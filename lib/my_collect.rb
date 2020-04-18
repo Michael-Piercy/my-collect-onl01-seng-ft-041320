c130213d12f96730544bb5d320c14889b60f80ec
Michael Piercy <michael.piercy@zoho.com>
2020-04-18 19:31:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,29 +3,7 @@ def my_collect(array)
   new_array = []
   while i < array.length
   new_array << yield (array[i])
-end
-
-
-
-
-def my_collect(languages, students)
-  
-  language = []
-  |
-  
-  language << yield (languages[i])
   i += 1
   end
-  language.upcase
-end
-
- 
-  student = []
-  
-  
-  student << yield (students[i])
-  i += 1
-  end
-  student
-  end
+  new_array
 end
